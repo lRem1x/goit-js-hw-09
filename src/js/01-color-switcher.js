3a632f171942ca1230885f806cb40232015ed44a
Roman Balakleec <lrem1x777@gmail.com>
2022-12-11 23:58:15 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startBtn = document.querySelector(`[data-start]`);
+const stopBtn = document.querySelector(`[data-stop]`);
+const bodyEl = document.querySelector(`body`);
+const boxEl = document.querySelector(`.box`);
 
+boxEl.style.display = `flex`;
+boxEl.style.justifyContent = `center`;
+
+
+startBtn.addEventListener(`click`, onClickStart);
+stopBtn.addEventListener(`click`, onClickStop);
+stopBtn.setAttribute("disabled", false); 
+
+
+let timerId = null;
+
+function onClickStart(e) {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    
+}
