e9df99bb170e0d969995321832cc725e8439d57f
Roman Balakleec <lrem1x777@gmail.com>
2022-12-14 21:56:46 +0200

    final hm9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,25 +1,31 @@
-const startBtn = document.querySelector(`[data-start]`);
-const stopBtn = document.querySelector(`[data-stop]`);
-const bodyEl = document.querySelector(`body`);
-const boxEl = document.querySelector(`.box`);
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+const boxEl = document.querySelector('.box');
+boxEl.style.display = 'flex';
+boxEl.style.justifyContent = 'center';
 
-boxEl.style.display = `flex`;
-boxEl.style.justifyContent = `center`;
 
-
-startBtn.addEventListener(`click`, onClickStart);
-stopBtn.addEventListener(`click`, onClickStop);
-stopBtn.setAttribute("disabled", false); 
-
+startBtn.addEventListener('click', onClickStart);
+stopBtn.addEventListener('click', onClickStop);
+stopBtn.setAttribute("disabled", false);
 
 let timerId = null;
 
 function onClickStart(e) {
     bodyEl.style.backgroundColor = getRandomHexColor();
-    timerId = setInterval(() => {
+    timerId = setInterval(() => { 
         bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.setAttribute("disabled", false);
+    stopBtn.removeAttribute("disabled");
+}
+function onClickStop(e) { 
+    clearInterval(timerId);
+    startBtn.removeAttribute("disabled");
+    stopBtn.setAttribute("disabled", false);
+}
 
-    }, 1000)
-
-    startBtn.setAttribute
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
+let intervalCounter = 0;
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  intervalCounter = 0;
+
+  const formData = new FormData(e.target);
+  const { delay, step, amount } = Object.fromEntries(formData);
+
+  let countertDeley = +delay;
+  let counterPosition = 0;
+
+    const intervalId = setInterval(() => {
+      intervalCounter += 1;
+      if (intervalCounter === +amount) {
+        clearInterval(intervalId);
+      };
+      if (intervalCounter === 1) {
+        counterPosition += 1;
+        createPromise(counterPosition, countertDeley).then(result => result).catch(result => result);
+        return
+      } else {
+        counterPosition += 1;
+        countertDeley += +step;
+        return createPromise(counterPosition, countertDeley).then(result => result).catch(result => result);
+      }
+    }, 0);
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promis = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`));
+      } else {
+        reject(Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
+      }
+    }, delay)
+  })
+  return promis;
 }
